642d4070c81c7a9e201756892b14d46793441036
lhulinsky <78326398+lhulinsky@users.noreply.github.com>
2023-10-27 17:23:18 -0500

    new song

--- ten minute timer.js
+++ ten minute timer.js
@@ -6,7 +6,7 @@ var lateStartClassEndTimes=[[11,0],[11,34],[12,8],[12,40],[13,14],[13,48],[14,22
 var alertMode=false;
 var bellOffset=Math.floor(getSchoolBellOffset());
 var dayType="normal";
-var alarmAudio=new Audio("Hot Wings (I Wanna Party).mp3");
+var alarmAudio=new Audio("Here Comes The Sun.mp3");
 var alarmHasRung=false;
 function changeDay(){
 	alarmHasRung=false;
